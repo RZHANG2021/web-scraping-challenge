6c45ac6b0325416821085d527e9a5e509b3a2fa6
Rachel Zhang <mailrenxu@hotmail.com>
2021-08-08 22:32:55 +1000

    updates on codes

--- scrape_mars.py
+++ scrape_mars.py
@@ -13,49 +13,69 @@ def scrape_all():
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=False)
 
+    Title, Article = mars_news(browser)
+    featured_image_url = featured_image(browser)
+    facts = mars_facts(browser)
+    hemisph = hemispheres(browser)
+    
     web_content={}
+    web_content['news_title'] = Title
+    web_content['news_paragraph'] = Article
+    web_content['image_url'] = featured_image_url
+    web_content['table'] = facts
+    web_content['hemisphere_image'] = hemisph
 
-    # # Scrape website to get NASA Mars News science
+    
+    
+    # stop webdriver and return data
+    browser.quit()
+    return web_content
+
+# # Scrape website mars news
+def mars_news(browser):
 
     #URL of page to be scraped
     article_url='https://redplanetscience.com/'
     browser.visit(article_url)
 
-
-
-    #create BeautifulSoup object;parse with 'html.parser'
+    #convert the broswer html to a soup object ;parse with 'html.parser'
     html=browser.html
     soup=bs(html,'html.parser')
 
     Title=soup.body.find_all('div',class_='content_title')[0].get_text()
- 
+
     Article=soup.body.find_all('div',class_='article_teaser_body')[0].get_text()
-    Article
 
 
-    # # Scrape website spaceimages-mars
+    return Title, Article
+
+
+
+# # Scrape website spaceimages-mars
+
+def featured_image(browser):
 
     #URL of page to be scraped
     fullimage_url='https://spaceimages-mars.com/'
     browser.visit(fullimage_url)
 
-
-
     html=browser.html
     soup=bs(html,'html.parser')
-
-    button = browser.links.find_by_partial_text('FULL IMAGE')
+    
     # Interact with elements
+    button = browser.links.find_by_partial_text('FULL IMAGE')
+    
     button.click()
-
 
     link=soup.find('img', class_='headerimage fade-in')['src']
     featured_image_url=fullimage_url+link
-    # print(featured_image_url)
+    
+    return featured_image_url
 
 
-    # # Scrape Mars Facts
+# # Scrape Mars Facts
 
+def mars_facts(browser):
 
     #connet to Mars Facts
     facts_url = "https://galaxyfacts-mars.com/"
@@ -65,16 +85,16 @@ def scrape_all():
     html = browser.html
     soup = bs(html, "html.parser")
     mars_facts = pd.read_html("https://space-facts.com/mars/")[1]
-    # print(mars_facts)
-
-
-
-    # mars_facts.set_index(['Mars - Earth Comparison'])
     mars_facts= mars_facts.set_index(['Mars - Earth Comparison'])
-    # print(mars_facts)
-
     facts = mars_facts.to_html()
-    # print(facts)
+    return facts
+
+# #  Scrape website marshemispheres
+
+def hemispheres(broswer):
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
     #URL of page to be scraped
     marshemispheres_url="https://marshemispheres.com/index.html"
@@ -86,10 +106,9 @@ def scrape_all():
     links=soup.find_all('h3')
     len(links)
 
+
     # set up a empty list to store individual dictionaries
     hemisphere_image_urls = []
-
-
 
     # loop through the website to collect each link and image url
     for i in range(len(links)):
@@ -99,49 +118,35 @@ def scrape_all():
             images[i].click()
             html=browser.html
             soup=bs(html,'html.parser')
-        
-        #  find image urls  
+
+            # find image urls  
             image_urls="https://marshemispheres.com/"+soup.find('img', class_='wide-image')['src']
 
-        # find image titles     
+            # find image titles     
             title=soup.find('h2').get_text()
 
-        
+
             hemisphere_image_url={"title":title,"image_url":image_urls}  
-        # print (hemisphere_image_url)
-        
-        # set up a dictionarie and store title and img_url
+            print (hemisphere_image_url)
+
+            # set up a dictionarie and store title and img_url
             hemispheres = {"title": title,"img_url": image_urls}        
 
-        # append individual hemispheres into a list
-            hemisphere_image_urls.append(hemispheres )
-        
+            # append individual hemispheres into a list
+            hemisphere_image_urls.append(hemispheres)
 
-        #  click back to the initial website
+
+            #click back to the initial website
             browser.back()
-    
+
         except:
             print("item is not found")
 
+    return hemisphere_image_urls
+
+if __name__ == "__main__":
+    print(scrape_all())
+    
 
 
-    # web_content =   {
-    #         "news_title": Title,
-    #         "news_paragraph": Article,
-    #         "featured_image": featured_image_url,
-    #         "facts": facts,
-    #         "hemispheres": hemisphere_image_urls
-    # }
-    
-    web_content['news_title'] = Title
-    web_content['news_paragraph'] = Article
-    web_content['image_url'] = featured_image_url
-    web_content['table'] = facts
-    web_content['hemisphere_image'] = hemisphere_image_urls
-    
-    # stop webdriver and return data
-    browser.quit()
-    
-    # browser.quit()
-    
-    return web_content
+
